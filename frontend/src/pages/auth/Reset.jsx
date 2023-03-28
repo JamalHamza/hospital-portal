29b9cda0694e8daafbf2f47324fd11430336228f
JamalHamza <95174044+JamalHamza@users.noreply.github.com>
2023-03-29 00:26:37 +0300

    PasswordStength.jsx

--- frontend/src/pages/auth/Reset.jsx
+++ frontend/src/pages/auth/Reset.jsx
@@ -1,35 +1,96 @@
+import {
+  Box,
+  Button,
+  IconButton,
+  InputAdornment,
+  TextField,
+} from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { MdOutlinePassword } from 'react-icons/md';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import * as Yup from 'yup';
 import Card from '../../components/card/Card';
 import Loader from '../../components/loader/Loader';
-import Password from '../../components/showPassword/Password';
 import { RESET, resetPassword } from '../../redux/features/auth/authSlice';
 import styles from './auth.module.scss';
 
-const initialState = {
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+
+import HomeIcon from '@mui/icons-material/Home';
+import LoginIcon from '@mui/icons-material/Login';
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
+import { useFormik } from 'formik';
+import { ImCheckmark, ImCross } from 'react-icons/im';
+import PasswordStrength from '../../components/passwordStrength/PasswordStrength';
+
+const initialValues = {
   password: '',
   password2: '',
 };
 
+// ! ------ Yup Validation ------------------
+const validationSchema = Yup.object().shape({
+  password: Yup.string()
+    .min(6, 'Password must be at least 6 characters')
+    .required('Password is required'),
+  password2: Yup.string()
+    .oneOf([Yup.ref('password'), null], 'Passwords must match')
+    .required('Confirm Password is required'),
+});
+
 function Reset() {
-  const [formData, setFormData] = useState(initialState);
+  const [formData, setFormData] = useState(initialValues);
   const { password, password2 } = formData;
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPassword2, setShowPassword2] = useState(false);
+
   const { isLoading, message, isLoggedIn, isSuccess } = useSelector(
     (state) => state.auth
   );
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { resetToken } = useParams();
 
-  console.log(typeof resetToken);
+  // console.log(typeof resetToken);
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
+  const togglePassword2 = () => {
+    setShowPassword2(!showPassword2);
+  };
+
+  const togglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    formik.setFieldValue(name, value);
     setFormData({ ...formData, [name]: value });
   };
+
+  // ! ------- Password Strength Indicator ------------
+  const [uCase, setUCase] = useState(false);
+  const [num, setNum] = useState(false);
+  const [sChar, setSChar] = useState(false);
+  const [passLength, setPassLength] = useState(false);
+  const [passMatch, setPassMatch] = useState(false);
+  // ! ---- Dynamic function for passwrod stength ----
+  const timesIcon = <ImCross size={8} color='red' />;
+  const checkIcon = <ImCheckmark size={8} color='green' />;
+  ImCross;
+
+  const switchIcon = (condition) => {
+    if (condition) {
+      return checkIcon;
+    } else {
+      return timesIcon;
+    }
+  };
+
+  // ! ----- Reset Function -----------------------
 
   const reset = async (e) => {
     e.preventDefault();
@@ -40,7 +101,7 @@ function Reset() {
       return toast.error('Password must to be up to 6 characters');
     }
 
-    const userData = { 
+    const userData = {
       password,
     };
 
@@ -48,6 +109,8 @@ function Reset() {
     await dispatch(RESET(userData));
     navigate('/login');
   };
+
+  // ! -----------------------------------------
 
   useEffect(() => {
     if (isSuccess && message.includes('Reset Successful')) {
@@ -57,7 +120,53 @@ function Reset() {
     dispatch(RESET());
   }, [dispatch, navigate, message, isSuccess]);
 
-  // ! --------------
+  // ! ----- password & password2 check -------
+  useEffect(() => {
+    // Check Lower and Uppercase
+    if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) {
+      setUCase(true);
+    } else {
+      setUCase(false);
+    }
+    // Check for numbers
+    if (password.match(/([0-9])/)) {
+      setNum(true);
+    } else {
+      setNum(false);
+    }
+    // Check for special character
+    if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)) {
+      setSChar(true);
+    } else {
+      setSChar(false);
+    }
+    // Check for PASSWORD LENGTH
+    if (password.length > 6) {
+      setPassLength(true);
+    } else {
+      setPassLength(false);
+    }
+    // Check for Password Match
+    if (password === password2 && password.length > 0 && password2.length > 0) {
+      setPassMatch(true);
+    } else {
+      setPassMatch(false);
+    }
+  }, [password, password2]);
+
+  // ! --------------------------------------
+
+  const formik = useFormik({
+    initialValues: initialValues,
+    validationSchema: validationSchema,
+    onSubmit: (values, { setSubmitting }) => {
+      // Handle form submission here
+      console.log(values);
+      reset(values);
+    },
+  });
+
+  // ! ---------------------------------------
   return (
     <div className={`container ${styles.auth}`}>
       {isLoading ? (
@@ -70,30 +179,143 @@ function Reset() {
             </div>
             <h2>Reset Password</h2>
             <form onSubmit={reset} noValidate>
-              <Password
-                placeholder='New Password'
+              <TextField
                 name='password'
-                value={password}
-                onChange={handleInputChange}
+                type={showPassword ? 'text' : 'password'}
+                label='New Password'
+                onChange={handleChange}
+                value={formik.values.password}
+                style={{ margin: '8px', width: '100%' }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position='end'>
+                      <IconButton onClick={togglePassword}>
+                        {showPassword ? (
+                          <VisibilityIcon />
+                        ) : (
+                          <VisibilityOffIcon />
+                        )}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+                onBlur={formik.handleBlur}
+                error={
+                  formik.touched.password && Boolean(formik.errors.password)
+                }
+                helperText={formik.touched.password && formik.errors.password}
               />
-              <Password
-                placeholder='Confirm Password'
+              <TextField
                 name='password2'
-                value={password2}
-                onChange={handleInputChange}
+                type={showPassword2 ? 'text' : 'password'}
+                label='Confirm New Password'
+                onChange={handleChange}
+                value={formik.values.password2}
+                style={{ margin: '8px', width: '100%' }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position='end'>
+                      <IconButton onClick={togglePassword2}>
+                        {showPassword2 ? (
+                          <VisibilityIcon />
+                        ) : (
+                          <VisibilityOffIcon />
+                        )}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+                onBlur={formik.handleBlur}
+                error={
+                  formik.touched.password && Boolean(formik.errors.password)
+                }
+                helperText={formik.touched.password && formik.errors.password}
               />
 
-              <button type='submit' className='--btn --btn-primary --btn-block'>
-                Get Reset Email
-              </button>
-              <div className={styles.links}>
-                <p>
-                  <Link to='/'>-Home</Link>
-                </p>
-                <p>
-                  <Link to='/login'> &nbsp; -Login</Link>
-                </p>
-              </div>
+              <Button
+                type='submit'
+                variant='contained'
+                sx={{
+                  bgcolor: 'third.main',
+                  margin: '0.8em',
+                  padding: '0.8em 2em',
+                  fontWeight: 800,
+                  fontSize: '1.2rem',
+                  color: 'primary.dark',
+                  width: '100%',
+                  '&:hover': {
+                    background: '#ccc6b4',
+                  },
+                }}
+              >
+                Reset Password
+              </Button>
+              {/* <Card cardclass={styles.group}>
+                <ul className='form-list'>
+                  <li>
+                    <span className={styles.indicator}>
+                      {switchIcon(uCase)}
+                      &nbsp; Lowercase & UpperCase
+                    </span>
+                  </li>
+                  <li>
+                    <span className={styles.indicator}>
+                      {switchIcon(num)}
+                      &nbsp; Number (0-9)
+                    </span>
+                  </li>
+                  <li>
+                    <span className={styles.indicator}>
+                      {switchIcon(sChar)}
+                      &nbsp; Special Character(!@#$%^&*)
+                    </span>
+                  </li>
+                  <li>
+                    <span className={styles.indicator}>
+                      {switchIcon(passLength)}
+                      &nbsp; At least 6 Character
+                    </span>
+                  </li>
+                  <li>
+                    <span className={styles.indicator}>
+                      {switchIcon(passMatch)}
+                      &nbsp; Password Match
+                    </span>
+                  </li>
+                </ul>
+              </Card> */}
+              <PasswordStrength password={password} password2={password2} />
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  margin: '0.4em',
+                }}
+              >
+                <Link to='/'>
+                  <IconButton>
+                    <HomeIcon fontSize='large' sx={{ color: 'primary.main' }} />
+                  </IconButton>
+                </Link>
+
+                <Link to='/register'>
+                  <IconButton>
+                    <PersonAddAltIcon
+                      fontSize='large'
+                      sx={{ color: 'primary.main' }}
+                    />
+                  </IconButton>
+                </Link>
+                <Link to='/login'>
+                  <IconButton>
+                    <LoginIcon
+                      fontSize='large'
+                      sx={{ color: 'primary.main' }}
+                    />
+                  </IconButton>
+                </Link>
+              </Box>
             </form>
           </div>
         </Card>
