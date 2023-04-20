3f6da15cb3d67ec2287a989b62c78415df93b047
JamalHamza <95174044+JamalHamza@users.noreply.github.com>
2023-04-20 03:04:55 +0300

    PersonalInfoAvatar.jsx created

--- frontend/src/components/doctors/doctor/personalInfo/PersonalInfo.jsx
+++ frontend/src/components/doctors/doctor/personalInfo/PersonalInfo.jsx
@@ -1,15 +1,11 @@
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import CurrencyRubleIcon from '@mui/icons-material/CurrencyRuble';
 import EmailIcon from '@mui/icons-material/Email';
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import ScienceIcon from '@mui/icons-material/Science';
 import StarsIcon from '@mui/icons-material/Stars';
-import { Box, Grid, IconButton, Typography } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 import moment from 'moment';
 import React from 'react';
-import { AiFillDelete } from 'react-icons/ai';
-import { GoVerified } from 'react-icons/go';
-import { RiErrorWarningLine } from 'react-icons/ri';
 import { useDispatch } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import {
@@ -18,13 +14,13 @@ import {
 } from '../../../../redux/features/booking/bookingSlice';
 import LiveDate from '../liveDate/LiveDate';
 import './PersonalInfo.css';
+import PersonalInfoAvatar from './personalInfoAvatar/PersonalInfoAvatar';
 
 function PersonalInfo(props) {
   const { doctor } = props;
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { id } = useParams();
-  console.log(id);
   const formatedDate = moment.utc(doctor?.createdAt).format('YYYY-MM-DD');
   // ! Count Doctor experinces from array
   const experienceDoctor = doctor?.experiences?.reduce((total, exp) => {
@@ -41,62 +37,7 @@ function PersonalInfo(props) {
     <>
       <Box>
         <Grid container>
-          {/*------- Personal Info Avatar ------------------ */}
-          <Grid container>
-            <Grid item xs={10} md={11.5}>
-              <IconButton onClick={() => navigate('/admin/doctors')}>
-                <ArrowBackIcon sx={{ color: 'third.dark', fontSize: '3rem' }} />
-              </IconButton>
-            </Grid>
-            <Grid item xs={2} md={0.5}>
-              <IconButton onClick={() => handleDelete(id)}>
-                <AiFillDelete fontSize={24} color='red' />
-              </IconButton>
-            </Grid>
-          </Grid>
-          <Grid item xs={12} sm={6} md={6}>
-            <Box
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                gap: '0.4em',
-                mb: '1em',
-              }}
-            >
-              <img
-                className='doctor-profile-img'
-                src={doctor?.photo}
-                alt={doctor?.name}
-              />
-              <Box>
-                {doctor?.isVerified ? (
-                  <Box
-                    sx={{
-                      display: 'flex',
-                    }}
-                  >
-                    <GoVerified fontSize={20} color='green' />
-                    <Typography variant='h6' sx={{ color: 'green' }}>
-                      Verified
-                    </Typography>
-                  </Box>
-                ) : (
-                  <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                    <RiErrorWarningLine fontSize={20} color='red' />
-                    <Typography variant='h6' sx={{ color: 'third.dark' }}>
-                      Not Verified
-                    </Typography>
-                  </Box>
-                )}
-              </Box>
-              <Typography
-                variant='body1'
-                sx={{ color: 'secondary.dark', ml: '1em' }}
-              >{`Created at ${formatedDate}`}</Typography>
-            </Box>
-          </Grid>
+          <PersonalInfoAvatar doctor={doctor} formatedDate={formatedDate} />
           {/* -------------Personal InfoBody--------------------- */}
           <Grid item xs={12} sm={6} md={4}>
             {/* ---------------------------------------------- */}
---
+++ frontend/src/components/doctors/doctor/personalInfo/personalInfoAvatar/PersonalInfoAvatar.jsx
@@ -0,0 +1,70 @@
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Box, Grid, IconButton, Typography } from '@mui/material';
+import { AiFillDelete } from 'react-icons/ai';
+import { GoVerified } from 'react-icons/go';
+import { RiErrorWarningLine } from 'react-icons/ri';
+import '../PersonalInfo.css';
+
+function PersonalInfoAvatar({ doctor, formatedDate }) {
+  return (
+    <>
+      <Grid container>
+        <Grid item xs={10} md={11.5}>
+          <IconButton onClick={() => navigate('/admin/doctors')}>
+            <ArrowBackIcon sx={{ color: 'third.dark', fontSize: '3rem' }} />
+          </IconButton>
+        </Grid>
+        <Grid item xs={2} md={0.5}>
+          <IconButton onClick={() => handleDelete(id)}>
+            <AiFillDelete fontSize={24} color='red' />
+          </IconButton>
+        </Grid>
+      </Grid>
+      <Grid item xs={12} sm={6} md={6}>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: '0.4em',
+            mb: '1em',
+          }}
+        >
+          <img
+            className='doctor-profile-img'
+            src={doctor?.photo}
+            alt={doctor?.name}
+          />
+          <Box>
+            {doctor?.isVerified ? (
+              <Box
+                sx={{
+                  display: 'flex',
+                }}
+              >
+                <GoVerified fontSize={20} color='green' />
+                <Typography variant='h6' sx={{ color: 'green' }}>
+                  Verified
+                </Typography>
+              </Box>
+            ) : (
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <RiErrorWarningLine fontSize={20} color='red' />
+                <Typography variant='h6' sx={{ color: 'third.dark' }}>
+                  Not Verified
+                </Typography>
+              </Box>
+            )}
+          </Box>
+          <Typography
+            variant='body1'
+            sx={{ color: 'secondary.dark', ml: '1em' }}
+          >{`Created at ${formatedDate}`}</Typography>
+        </Box>
+      </Grid>
+    </>
+  );
+}
+
+export default PersonalInfoAvatar;
