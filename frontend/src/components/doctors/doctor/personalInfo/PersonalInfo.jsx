0cceca28ab3e7d4ccbcf2c53b7463cb455b2f254
JamalHamza <95174044+JamalHamza@users.noreply.github.com>
2023-04-20 03:17:19 +0300

    PersonalInfoBody.jsx created

--- frontend/src/components/doctors/doctor/personalInfo/PersonalInfo.jsx
+++ frontend/src/components/doctors/doctor/personalInfo/PersonalInfo.jsx
@@ -1,9 +1,4 @@
-import CurrencyRubleIcon from '@mui/icons-material/CurrencyRuble';
-import EmailIcon from '@mui/icons-material/Email';
-import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
-import ScienceIcon from '@mui/icons-material/Science';
-import StarsIcon from '@mui/icons-material/Stars';
-import { Box, Grid, Typography } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import moment from 'moment';
 import React from 'react';
 import { useDispatch } from 'react-redux';
@@ -12,9 +7,9 @@ import {
   deleteDoctor,
   getDoctors,
 } from '../../../../redux/features/booking/bookingSlice';
-import LiveDate from '../liveDate/LiveDate';
 import './PersonalInfo.css';
 import PersonalInfoAvatar from './personalInfoAvatar/PersonalInfoAvatar';
+import PersonalInfoBody from './personalInfoBody/PersonalInfoBody';
 
 function PersonalInfo(props) {
   const { doctor } = props;
@@ -43,121 +38,10 @@ function PersonalInfo(props) {
             handleDelete={handleDelete}
             doctorId={id}
           />
-          {/* -------------Personal InfoBody--------------------- */}
-          <Grid item xs={12} sm={6} md={4}>
-            {/* ---------------------------------------------- */}
-            <Grid item xs={12} sm={12} md={12}>
-              <Box
-                sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                }}
-              >
-                <Typography
-                  variant='h4'
-                  sx={{ color: 'primary.main', fontWeight: '600', mb: '0.5em' }}
-                >
-                  {doctor?.name}
-                </Typography>
-                <Box
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                  }}
-                >
-                  <EmailIcon fontSize='large' sx={{ color: 'primary.main' }} />
-                  <Typography
-                    variant='h5'
-                    sx={{
-                      ml: '0.5em',
-                      color: 'secondary.dark',
-                    }}
-                  >
-                    {doctor?.email}
-                  </Typography>
-                </Box>
-                <Box
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                  }}
-                >
-                  <LocalPhoneIcon
-                    fontSize='large'
-                    sx={{ color: 'primary.main' }}
-                  />
-                  <Typography
-                    variant='h5'
-                    sx={{ ml: '0.5em', color: 'secondary.dark' }}
-                  >
-                    {doctor?.phone}
-                  </Typography>
-                </Box>
-                <Box
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                  }}
-                >
-                  <CurrencyRubleIcon
-                    fontSize='large'
-                    sx={{ color: 'primary.main' }}
-                  />
-                  <Typography
-                    variant='h5'
-                    sx={{ ml: '0.5em', color: 'secondary.dark' }}
-                  >
-                    {doctor?.fee}
-                  </Typography>
-                </Box>
-                <Box
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                  }}
-                >
-                  <StarsIcon fontSize='large' sx={{ color: 'primary.main' }} />
-                  <Typography
-                    variant='h5'
-                    sx={{ ml: '0.5em', color: 'secondary.dark' }}
-                  >
-                    {doctor?.specialist}
-                  </Typography>
-                </Box>
-                <Box
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                  }}
-                >
-                  <ScienceIcon
-                    fontSize='large'
-                    sx={{ color: 'primary.main' }}
-                  />
-                  <Typography
-                    variant='h5'
-                    sx={{
-                      ml: '0.5em',
-                      color: 'secondary.dark',
-                      fontWeight: '700',
-                    }}
-                  >
-                    {experienceDoctor} Years Experience
-                  </Typography>
-                </Box>
-              </Box>
-            </Grid>
-            <Grid item xs={12} md={12}>
-              <LiveDate />
-            </Grid>
-          </Grid>
-          {/* ------------------------------------------------ */}
+          <PersonalInfoBody
+            doctor={doctor}
+            experienceDoctor={experienceDoctor}
+          />
         </Grid>
       </Box>
     </>
