feb93a20167bea443478c7264c159f4c19b0cab8
JamalHamza <95174044+JamalHamza@users.noreply.github.com>
2023-04-22 01:26:40 +0300

    DoctorList searchBar completed

--- frontend/src/components/doctors/doctorSearchbar/DoctorsSearchbar.jsx
+++ frontend/src/components/doctors/doctorSearchbar/DoctorsSearchbar.jsx
@@ -1,9 +1,55 @@
-import React from 'react';
+import { Grid, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { FILTER_DOCTORS } from '../../../redux/features/auth/filterSlice';
+import { getDoctors } from '../../../redux/features/booking/bookingSlice';
+import Search from '../../search/Search';
 
-function DoctorsSearchbar() {
+function DoctorsSearchBar() {
+  const [search, setSearch] = useState('');
+  const dispatch = useDispatch();
+  const { doctor, isLoading, doctors } = useSelector((state) => state.booking);
+  // ! doctors length
+  const allDoctorsNumber = doctors?.length;
+
+  // ! ----------------
+
+  useEffect(() => {
+    dispatch(getDoctors());
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(FILTER_DOCTORS({ doctors, search }));
+  }, [dispatch, doctors, search]);
+
   return (
-    <h3 style={{ border: '1px solid red', minHeight: '5vh' }}>Doctors Searchbar</h3>
+    <>
+      {isLoading ? null : (
+        <Grid
+          container
+          sx={{
+            minHeight: '4vh',
+            alignItems: 'center',
+          }}
+        >
+          <Grid item xs={12} md={8}>
+            <Typography
+              variant='h5'
+              sx={{ fontWeight: '700', color: 'primary.main' }}
+            >
+              Total Available Doctors: {allDoctorsNumber}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Search
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </Grid>
+        </Grid>
+      )}
+    </>
   );
 }
 
-export default DoctorsSearchbar;
+export default DoctorsSearchBar;
--- frontend/src/components/doctors/doctorsList/DoctorList.jsx
+++ frontend/src/components/doctors/doctorsList/DoctorList.jsx
@@ -12,6 +12,7 @@ import Card from '@mui/material/Card';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { selectorDoctors } from '../../../redux/features/auth/filterSlice';
 import { getDoctors } from '../../../redux/features/booking/bookingSlice';
 import Loader from '../../loader/Loader';
 import './DoctorList.css';
@@ -20,6 +21,8 @@ function DoctorList() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { isLoading, doctors } = useSelector((state) => state.booking);
+  const filteredDoctors = useSelector(selectorDoctors);
+  console.log(filteredDoctors);
   const handleClick = (id) => {
     navigate(`/admin/doctors/${id}`);
   };
@@ -32,108 +35,117 @@ function DoctorList() {
     <>
       {isLoading && <Loader />}
       <Grid container spacing={2}>
-        {doctors.map((doctor) => {
-          return (
-            <Grid item xs={12} sm={6} md={6} key={doctor._id}>
-              <Card
-                sx={{
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  bgcolor: 'fourth.lighter',
-                  maxHeight: '17rem',
-                  height: '100%',
-                }}
-              >
-                <Box
+        {!isLoading && filteredDoctors.length == 0 ? (
+          <Typography
+            variant='h5'
+            sx={{ m: '0 auto', p: '1em', color: 'third.dark' }}
+          >
+            No Doctor Found
+          </Typography>
+        ) : (
+          filteredDoctors.map((doctor) => {
+            return (
+              <Grid item xs={12} sm={6} md={6} key={doctor._id}>
+                <Card
                   sx={{
                     display: 'flex',
-                    flexDirection: 'column',
-                    width: '60%',
+                    justifyContent: 'space-between',
+                    bgcolor: 'fourth.lighter',
+                    maxHeight: '17rem',
                     height: '100%',
                   }}
                 >
-                  <CardContent
+                  <Box
                     sx={{
-                      width: '100%',
                       display: 'flex',
                       flexDirection: 'column',
-                      justifyContent: 'center',
-                      gap: '0.5em',
+                      width: '60%',
+                      height: '100%',
                     }}
                   >
-                    <Typography
-                      variant='h5'
+                    <CardContent
                       sx={{
-                        color: 'primary.dark',
-                        fontWeight: '700',
-                        letterSpacing: '1px',
-                      }}
-                    >
-                      {doctor.name}
-                    </Typography>
-                    <Typography
-                      variant='body1'
-                      color='text.primary'
-                      sx={{
+                        width: '100%',
                         display: 'flex',
-                        alignItems: 'center',
-                        color: 'secondary.dark',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        gap: '0.5em',
                       }}
                     >
-                      <EmailIcon
-                        fontSize='medium'
-                        sx={{ color: 'primary.dark', mr: '0.2em' }}
-                      />
-                      {doctor?.email}
-                    </Typography>
-                    <Typography
-                      variant='body2'
-                      color='text.primary'
-                      sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        color: 'secondary.dark',
-                      }}
-                    >
-                      <LocalHospitalIcon
-                        fontSize='medium'
-                        sx={{ color: 'primary.dark', mr: '0.2em' }}
-                      />
-                      {doctor?.specialist}
-                    </Typography>
-                    <Button
-                      onClick={() => handleClick(doctor._id)}
-                      variant='contained'
-                      sx={{
-                        borderRadius: '10px',
-                        padding: '6px 16px',
-                        fontWeight: 'bold',
-                        fontSize: '1.2rem',
-                        width: '12rem',
-                        color: 'primary.main',
-                        bgcolor: 'third.main',
-                        textTransform: 'capitalize',
-                        m: '1em 0',
-                        '&:hover': {
-                          backgroundColor: '#ccb7c0',
-                          color: '#fff',
-                        },
-                      }}
-                    >
-                      More details
-                    </Button>
-                  </CardContent>
-                </Box>
-                <CardMedia
-                  component='img'
-                  sx={{ width: '40%', height: '100%' }}
-                  image={doctor?.photo}
-                  alt='doctor-photo'
-                />
-              </Card>
-            </Grid>
-          );
-        })}
+                      <Typography
+                        variant='h5'
+                        sx={{
+                          color: 'primary.dark',
+                          fontWeight: '700',
+                          letterSpacing: '1px',
+                        }}
+                      >
+                        {doctor.name}
+                      </Typography>
+                      <Typography
+                        variant='body1'
+                        color='text.primary'
+                        sx={{
+                          display: 'flex',
+                          alignItems: 'center',
+                          color: 'secondary.dark',
+                        }}
+                      >
+                        <EmailIcon
+                          fontSize='medium'
+                          sx={{ color: 'primary.dark', mr: '0.2em' }}
+                        />
+                        {doctor?.email}
+                      </Typography>
+                      <Typography
+                        variant='body2'
+                        color='text.primary'
+                        sx={{
+                          display: 'flex',
+                          alignItems: 'center',
+                          color: 'secondary.dark',
+                        }}
+                      >
+                        <LocalHospitalIcon
+                          fontSize='medium'
+                          sx={{ color: 'primary.dark', mr: '0.2em' }}
+                        />
+                        {doctor?.specialist}
+                      </Typography>
+                      <Button
+                        onClick={() => handleClick(doctor._id)}
+                        variant='contained'
+                        sx={{
+                          borderRadius: '10px',
+                          padding: '6px 16px',
+                          fontWeight: 'bold',
+                          fontSize: '1.2rem',
+                          width: '12rem',
+                          color: 'primary.main',
+                          bgcolor: 'third.main',
+                          textTransform: 'capitalize',
+                          m: '1em 0',
+                          '&:hover': {
+                            backgroundColor: '#ccb7c0',
+                            color: '#fff',
+                          },
+                        }}
+                      >
+                        More details
+                      </Button>
+                    </CardContent>
+                  </Box>
+                  <CardMedia
+                    component='img'
+                    sx={{ width: '40%', height: '100%' }}
+                    image={doctor?.photo}
+                    alt='doctor-photo'
+                  />
+                </Card>
+              </Grid>
+            );
+          })
+        )}
       </Grid>
     </>
   );
