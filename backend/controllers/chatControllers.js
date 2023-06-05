fa1ee1d997e57a93c1cef57d14e7013884b7109c
JamalHamza <95174044+JamalHamza@users.noreply.github.com>
2023-06-05 13:36:27 +0300

    chat completed backend

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -20,7 +20,7 @@ const accessChat = asyncHandler(async (req, res) => {
     ],
   })
     .populate('users', '-password')
-    .populate('lastMessage');
+    .populate('latestMessage');
 
   // ! -----------------------------
   isChat = await User.populate(isChat, {
