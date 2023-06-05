9614e17ce58db56d74a87754bad0db035163e2d5
JamalHamza <95174044+JamalHamza@users.noreply.github.com>
2023-06-05 12:38:44 +0300

    accessChat routes

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -1,6 +1,57 @@
 const asyncHandler = require('express-async-handler');
+const Chat = require('../models/chatModel');
+const User = require('../models/userModel');
 
 // * -------------------------------------------------
-const accessChat = asyncHandler(async (req, res) => {});
+const accessChat = asyncHandler(async (req, res) => {
+  const { userId } = req.body;
+  // ! If userId is undefined
+  if (!userId) {
+    console.log('userId is undefined');
+    res.status(400);
+    throw new Error('Something went wrong');
+  }
+  // ! Find chat --------------------
+  let isChat = await Chat.find({
+    $and: [
+      { users: { $elemMatch: { $eq: req.user._id } } },
+      { users: { $elemMatch: { $eq: userId } } },
+    ],
+  })
+    .populate('users', '-password')
+    .populate('lastMessage');
 
-module.exports = { accessChat };
+  // ! -----------------------------
+  isChat = await User.populate(isChat, {
+    path: 'latestMessage.sender',
+    select: 'name pic email',
+  });
+  // ! isChat exists
+  if (isChat.length > 0) {
+    res.status(200);
+    res.send(isChat[0]);
+  } else {
+    let chatData = {
+      chatName: 'sender',
+      isGroupChat: false,
+      users: [req.user._id, userId],
+    };
+
+    try {
+      const createdChat = await Chat.create(chatData);
+      const FullChat = await Chat.findOne({ _id: createdChat._id }).populate(
+        'users',
+        '-password'
+      );
+      res.status(201).send(FullChat);
+    } catch (error) {
+      res.status(400);
+      throw new Error(error.message);
+    }
+  }
+});
+
+// * ------------------------------------------------
+const fetchChats = (asyncHandler = async (req, res) => {});
+
+module.exports = { accessChat, fetchChats };
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { protect, doctorOrPatient } = require('../middleware/AuthMiddleware');
-const { accessChat } = require('../controllers/chatControllers');
+const { accessChat, fetchChats } = require('../controllers/chatControllers');
 
 const router = express.Router();
 
 router.route('/').post(protect, doctorOrPatient, accessChat);
+router.route('/').get(protect, doctorOrPatient, fetchChats);
 
 module.exports = router;
