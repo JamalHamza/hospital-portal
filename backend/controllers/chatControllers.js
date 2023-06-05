a4abb4691108002ca1057ec6ae2930f49f8eebef
JamalHamza <95174044+JamalHamza@users.noreply.github.com>
2023-06-05 13:00:09 +0300

    ..

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -52,6 +52,24 @@ const accessChat = asyncHandler(async (req, res) => {
 });
 
 // * ------------------------------------------------
-const fetchChats = (asyncHandler = async (req, res) => {});
+const fetchChats = asyncHandler(async (req, res) => {
+  try {
+    let results = await Chat.find({
+      users: { $elemMatch: { $eq: req.user._id } },
+    })
+      .populate('users', '-password')
+      .populate('groupAdmin', '-password')
+      .populate('latestMessage');
+
+    results = await User.populate(results, {
+      path: 'latestMessage.sender',
+      select: 'name, pic, email',
+    });
+    res.status(200).send(results);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
 
 module.exports = { accessChat, fetchChats };
