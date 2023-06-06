439ccf79aa69d65a2f013d964fc8508fb344ba65
JamalHamza <95174044+JamalHamza@users.noreply.github.com>
2023-06-06 13:01:57 +0300

    searchDoctor redux logic

--- frontend/src/redux/features/chat/chatServices.jsx
+++ frontend/src/redux/features/chat/chatServices.jsx
@@ -6,6 +6,16 @@ const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 // *-----------------------------
 
 // ! Search Chat/Create Chat ----
-const getChat = async (search) => {
-    const response = await axios.get(BACKEND_URL + '/chat')
-}
+const searchDoctor = async (search) => {
+  const response = await axios.get(
+    BACKEND_URL + `/chat/doctor?search=${search}`
+  );
+  return response.data;
+};
+
+// * -----------------------------
+const chatServices = {
+  searchDoctor,
+};
+
+export default chatServices;
--- frontend/src/redux/features/chat/chatSlice.jsx
+++ frontend/src/redux/features/chat/chatSlice.jsx
@@ -1,18 +1,53 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
+import chatServices from './chatServices';
 
 const initialState = {
+  isLoading: false,
   selectedChat: null,
+  doctors: [],
   chats: [],
   notification: [],
 };
+
+// ! Get Doctors ---------------
+export const searchDoctor = createAsyncThunk(
+  '/searchDoctor',
+  async (search, thunkAPI) => {
+    try {
+      return await chatServices.searchDoctor(search);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString() ||
+        response.message;
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
 
 const bookingSLice = createSlice({
   name: 'chat',
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder;
-    // ! ------
+    builder
+      // ! Search Doctor -----------
+      .addCase(searchDoctor.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(searchDoctor.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.doctors = action.payload;
+      })
+      .addCase(searchDoctor.rejected, (state, action) => {
+        state.isLoading = false;
+        state.message = action.payload;
+        toast.error(action.payload);
+      });
   },
 });
 
