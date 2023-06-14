57c39c798c39ed68cd475df3eba4737370e34c82
JamalHamza <95174044+JamalHamza@users.noreply.github.com>
2023-06-14 10:35:51 +0300

    ChangePassword

--- frontend/src/components/userStats/UserStats.jsx
+++ frontend/src/components/userStats/UserStats.jsx
@@ -10,17 +10,16 @@ import {
 import InfoBox from '../infoBox/InfoBox';
 import './UsersStats.scss';
 
-const icon1 = <GrGroup size={40} color='red' />;
-const icon2 = <FiUserCheck size={40} color='#fff' />;
-const icon3 = <FiUserMinus size={40} color='#fff' />;
-const icon4 = <BiUserX size={40} color='#fff' />;
+const icon1 = <GrGroup size={36} color='red' />;
+const icon2 = <FiUserCheck size={36} color='#fff' />;
+const icon3 = <FiUserMinus size={36} color='#fff' />;
+const icon4 = <BiUserX size={36} color='#fff' />;
 
 function UserStats() {
   const dispatch = useDispatch();
   const { verifiedUsers, users, suspendedUsers } = useSelector(
     (state) => state.auth
   );
-  console.log(verifiedUsers);
 
   useEffect(() => {
     dispatch(CALC_VERIFIED_USER());
